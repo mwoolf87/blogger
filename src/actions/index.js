3464f8ecb71d2749c2ecb76b3c892e73e43366b7
mwoolf87 <michael.j.woolf@gmail.com>
2020-06-11 22:03:06 -0400

    created async action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,9 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = async () => {
-  const response = await jsonPlaceholder.get("/posts");
-  return {
+export const fetchPosts = () => async (dispatch) => {
+  const promise = await jsonPlaceholder.get("/posts");
+  dispatch({
     type: "FETCH_POSTS",
-    payload: response,
-  };
+    payload: promise,
+  });
 };
