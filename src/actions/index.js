e01ac7407ea1e490a7a7024a6f9714f915a1f471
mwoolf87 <michael.j.woolf@gmail.com>
2020-06-15 20:50:46 -0400

    created postsReducer file and mapStateToProps

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,9 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => async (dispatch) => {
-  const promise = await jsonPlaceholder.get("/posts");
+  const response = await jsonPlaceholder.get("/posts");
   dispatch({
     type: "FETCH_POSTS",
-    payload: promise,
+    payload: response.data,
   });
 };
