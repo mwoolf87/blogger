ddc910e42f62eca60429e17b2ef806c67f52946b
mwoolf87 <michael.j.woolf@gmail.com>
2020-06-21 22:11:50 -0400

    utilize memoization

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,12 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => async (dispatch) => {
+  console.log("about to fetch post");
+
+  await dispatch(fetchPosts());
+  console.log("fetched post");
+};
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
@@ -15,3 +23,16 @@ export const fetchUser = (id) => async (dispatch) => {
     payload: response.data,
   });
 };
+
+//MEMOIZATION AND VIA LODASH
+// export const fetchUser = (id) => (dispatch) => {
+//   _fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({
+//     type: "FETCH_USER",
+//     payload: response.data,
+//   });
+// });
